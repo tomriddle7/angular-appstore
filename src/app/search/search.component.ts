a978c4669c07d47e63470f84aca36691b86996b4
tomriddle7 <tomriddle7@naver.com>
2020-05-26 11:33:53 +0900

    Add Search Function

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import{ HttpClient } from '@angular/common/http';
+
+interface Result {
+  results: object;
+}
 
 @Component({
   selector: 'app-search',
@@ -7,10 +12,29 @@ import{ HttpClient } from '@angular/common/http';
   styleUrls: ['./search.component.css']
 })
 export class searchComponent {
+  searchResult: any;
+  checkoutForm;
 
+  
   constructor(
+    private formBuilder: FormBuilder,
     public http: HttpClient,
-  ) { }
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      property: ''
+    });
+  }
+
+  onSubmit(customerData) {
+    // 주문 로직은 여기에 구현합니다.
+    this.checkoutForm.reset();
+    this.http
+      .get<Result>(`https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=${customerData.property}&country=kr&entity=software`)
+      .subscribe((appdata:Result) => {
+        this.searchResult = appdata.results;
+        //console.log(this.searchResult);
+    });
+  }
 
   iTune(url) {
     window.open(url, '_blank');
